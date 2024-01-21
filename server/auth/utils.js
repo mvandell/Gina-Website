432a29c8f220eb8fb24d239070dfe7b4ca003986
Marisa <marisavandellos@gmail.com>
2024-01-21 11:07:29 -0800

    fixed weird bug and updated package json

--- server/auth/utils.js
+++ server/auth/utils.js
@@ -1,47 +1,53 @@
-const {prisma} = require("../db/client");
-const jwt = require("jsonwebtoken");
-const {JWT_SECRET} = process.env;
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = process.env
 
-//Authorization Middleware
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+//<--------------------------------AUTHORIZATION MIDDLEWARE-------------------------------->
 const authMiddleware = async (req, res, next) => {
-    const prefix = 'Bearer ';
-    const auth = req.header('Authorization');
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
 
-    if (!auth) {
-        next();
-    } else if (auth.startsWith(prefix)) {
-        const token = auth.slice(prefix.length);
-        try {
-            const {id} = jwt.verify(token, JWT_SECRET);
+  if (!auth) {
+    // nothing to see here
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
 
-            if (id) {
-                req.user = await prisma.user.findUnique({
-                    where: {id}
-                });
-                next();
-            } else {
-                next({
-                    name: 'AuthorizationHeaderError',
-                    message: 'Authorization token malformed',
-                });
-            }
-        } catch ({name, message}) {
-            next({name, message});
-        }
-    } else {
-        next({
-            name: 'AuthorizationHeaderError',
-            message: `Authorization token must start with ${prefix}`,
+      if (id) {
+        req.user = await prisma.user.findUnique({
+          where: {
+            id
+          }
         });
+        next();
+      } else {
+        next({
+          name: 'AuthorizationHeaderError',
+          message: 'Authorization token malformed',
+        });
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
     }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
 };
 
-//No User Error
+//<--------------------------------NO USER ERROR-------------------------------->
+//If there is no user in a required user action, send error.
 const requireUser = (req, res, next) => {
-    if (!req.user) {
-        res.status(401).send("Sorry, you don't have permission to do that.")
-    }
-    next();
+  if (!req.user) {
+    res.status(401).send("Sorry, you need an account to do that. Please sign up first")
+  }
+  next();
 };
 
 module.exports = {
