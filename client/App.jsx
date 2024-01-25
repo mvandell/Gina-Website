a4d828bc230820558665c1ceb98829e5385b2148
Marisa <marisavandellos@gmail.com>
2024-01-25 14:36:36 -0800

    added background color and routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
 
-    return (
+  return (
+    <div className='gradient_background'>
       <div className="App">
         Hello world!
+        {/* <NavBar /> */}
+        <Routes>
+          {/* <Route path="/" element={} />
+          <Route path="/about" element={} />
+          <Route path="/calendar" element={} />
+          <Route path="/cm" element={} />
+          <Route path="/contact" element={} />
+          <Route path="/policy" element={} /> */}
+        </Routes>
       </div>
-    );
-  }
-  
-  export default App;
+    </div>
+  );
+}
+
+export default App;
