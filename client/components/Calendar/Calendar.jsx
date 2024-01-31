73104e014d0738d49ec4a0878c0a3b540e00712d
Marisa <marisavandellos@gmail.com>
2024-01-31 15:25:16 -0800

    installed dependencies for MUI calendar

---
+++ client/components/Calendar/Calendar.jsx
@@ -0,0 +1,17 @@
+
+
+//Getting started: https://mui.com/x/react-date-pickers/getting-started/
+//Day.js notation: https://day.js.org/docs/en/installation/installation
+
+//installed dependencies
+
+const Calendar = () => {
+    
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Calendar;
