6825524d8f3503146bf49666f5c358eaa7dcaad3
Marisa <marisavandellos@gmail.com>
2024-02-02 15:16:27 -0800

    Calendar renders properly

--- client/components/Calendar/TeachingCalendar.jsx
+++ client/components/Calendar/TeachingCalendar.jsx
@@ -1,9 +1,13 @@
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar' //https://github.com/jquense/react-big-calendar
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import format from 'date-fns/format' //https://date-fns.org/docs/Getting-Started
 import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
-import getDate from 'date-fns/getDay'
+import getDay from 'date-fns/getDay'
 import enUS from 'date-fns/locale/en-US'
+
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
 
 import { useGetDatesQuery } from '../../redux/api'
 
@@ -15,7 +19,7 @@ const localizer = dateFnsLocalizer({
     format,
     parse,
     startOfWeek,
-    getDate,
+    getDay,
     locales,
 })
 
@@ -29,17 +33,22 @@ const TeachingCalendar = () => {
         return <div>Sorry! There's a problem loading the info.</div>
     }
 
-    
+    console.log(data);
 
     return (
-        <div>
-            <Calendar
-                localizer={localizer}
-                events={data}
-                defaultView='month'
-                style={{ height: "100vh" }}
-                showMultiDayTimes
-            />
+        <div className='calendar'>
+            <Card sx={{m:10, p: 2}}>
+                {data &&
+                    <Calendar
+                        localizer={localizer}
+                        events={data}
+                        defaultView='month'
+                        defaultDate={new Date()}
+                        style={{ height: "60vh", width: "60vw" }}
+                        showMultiDayTimes
+                    />
+                }
+            </Card>
         </div>
     )
 }
